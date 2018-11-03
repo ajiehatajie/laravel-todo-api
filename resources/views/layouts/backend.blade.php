1e436589e7e2d72dada9131d2eaf4b7fb746616c
hatajie@gmail.com <Indosat2017*>
2018-11-03 10:32:19 +0700

    ok

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -76,10 +76,7 @@
 
         <hr/>
 
-        <div class="container">
-            &copy; {{ date('Y') }}. Created by <a href="http://www.appzcoder.com">AppzCoder</a>
-            <br/>
-        </div>
+        
 
     </div>
 
